a4c2ce197d4d6adfbd388517e057d06dec47064c
Vrindavan Sanap <vrindavansanap@gmail.com>
2024-07-24 19:33:02 +0530

    -

--- freq_words.cc
+++ freq_words.cc
@@ -9,7 +9,7 @@
 #include <map>
 #include <sstream>
 #include <string>
-
+#include <vector>
 using namespace std;
 
 void remove_trailing_punct(std::string &str) {
@@ -18,8 +18,12 @@ void remove_trailing_punct(std::string &str) {
   }
 }
 
-int main() {
+bool sort_by_second_desc(const pair<string, int> &a,
+                         const pair<string, int> &b) {
+  return a.second > b.second;
+}
 
+int main() {
   ifstream file;
   string file_name = "bee_movie.txt";
   file.open(file_name);
@@ -36,16 +40,34 @@ int main() {
   string word;
   int total_count = 0;
   while (getline(s_stream, word, ' ')) {
-    cout << word << endl;
     remove_trailing_punct(word);
-    map_[word] += 1;
+    if (word != "") {
+
+      map_[word] += 1;
+    }
     total_count++;
   }
+  /*
   for (const auto &pair : map_) {
     std::cout << pair.first << " -> " << pair.second << '\n';
   }
+  */
+  std::cout << "There are " << total_count << " (" << map_.size()
+            << ") unique words in " << file_name << endl;
 
-  cout << "There are " << total_count << " (" << map_.size()
-       << ") unique words in " << file_name << endl;
+  vector<pair<string, int>> freq_vector;
+
+  // Iterate through the map and push each key-value pair into the vector
+  for (const auto &pair : map_) {
+    freq_vector.push_back(pair);
+  }
+
+  sort(freq_vector.begin(), freq_vector.end(), sort_by_second_desc);
+  std::cout << "Top 10 most freqently used words are:\n";
+  for (int i = 0; i < 10; i++) {
+    auto p = freq_vector[i];
+    std::cout << p.first << " --> " << p.second << endl;
+  }
+
   return 0;
 }
