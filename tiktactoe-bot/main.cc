c6be8a95aee07a4d317c32f66411a930a8fcc25e
Vrindavan Sanap <vrindavansanap@gmail.com>
2025-10-06 23:59:09 +0530

    checking pre commit hooks

--- tiktactoe-bot/main.cc
+++ tiktactoe-bot/main.cc
@@ -6,7 +6,7 @@ using namespace std;
 int board_state[3][3] = 0;
 char int_to_peice(int peice_code){
   if (peice_code == 0){
-    return ' ';
+      return ' ';
   }else if (peice_code == -1){
     return 'X';
   }else if(peice_code == 1){
